22e73db1ca4796e36f34a23f7ff852f059831f93
guka20 <gukadavitadze23@gmail.com>
2023-09-22 19:03:05 +0400

    dishes pagination is done

--- src/pages/home/components/index.ts
+++ src/pages/home/components/index.ts
@@ -1,3 +1,4 @@
+export { DishesPagination } from "../../../components/DishesPagination/DishesPagination";
 export { CartSwiper } from "./SwiperSlider/CartSwiper";
 export { MiniProductCard } from "./MiniProductCard/MiniProductCard";
 export { Header } from "./Header/Header";
