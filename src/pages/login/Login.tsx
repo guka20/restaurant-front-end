b42341f266a78e354fa8cd2cf801ab47064e4201
guka20 <gukadavitadze23@gmail.com>
2023-09-25 16:05:43 +0400

    login-page is done

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputElement, MainButton } from "src/components";
+import icon from "src/assets/logo/chef.png";
+import "./Login.css";
+type LoginPropertiesTypes = {
+  email: string;
+  password: string;
+};
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [loginProperties, setLoginProperties] = useState<LoginPropertiesTypes>({
+    email: "",
+    password: "",
+  });
+  const handleValueChange = (e: any) => {
+    const { value, name } = e.target;
+
+    setLoginProperties({
+      ...loginProperties,
+      [name]: value,
+    });
+  };
+  const handleLoginClick = () => {};
+  const handleSignUpClick = () => {};
+  return (
+    <div className="login-page">
+      <div className="image">
+        <img src={icon} alt="" width={50} />
+      </div>
+      <div className="login-elements">
+        <InputElement
+          name="email"
+          type="email"
+          placeholder="Email address"
+          value={loginProperties?.email}
+          handleContactValueChange={handleValueChange}
+        />
+        <InputElement
+          name="password"
+          type="email"
+          placeholder="Password"
+          value={loginProperties?.password}
+          handleContactValueChange={handleValueChange}
+        />
+
+        <div className="forgot-password">Forgot password</div>
+
+        <MainButton handleButtonClick={handleLoginClick}>SIGN IN</MainButton>
+
+        <div className="line"></div>
+
+        <MainButton handleButtonClick={handleSignUpClick}>SIGN UP</MainButton>
+      </div>
+    </div>
+  );
 };
