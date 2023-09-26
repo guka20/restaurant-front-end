a51814371cce25cc49aad1622f4dce97a003bf81
guka20 <gukadavitadze23@gmail.com>
2023-09-26 22:33:35 +0400

    add protection on navbar

--- src/components/ProductCart/ProductCart.tsx
+++ src/components/ProductCart/ProductCart.tsx
@@ -3,14 +3,24 @@ import "./ProductCart.css";
 import { BsCartPlus } from "react-icons/bs";
 import img from "src/assets/products/cake.png";
 import { ProductCardTypes } from "src/types/Types";
+import { useUserTypeContext } from "src/context";
+import { UserEnum } from "src/types/user.types";
+import { useNavigate } from "react-router-dom";
 export const ProductCart = ({ title, subtitle, price }: ProductCardTypes) => {
+  const { userType } = useUserTypeContext();
+  const navigate = useNavigate();
+  const handleAddInCartButton = () => {
+    if (userType === UserEnum.GUEST) {
+      navigate("/login");
+    }
+  };
   return (
     <div className="product-cart">
       <div className="image-cart-place">
         <div className="image-place">
           <img src={img} alt="" />
         </div>
-        <button className="add-to-cart">
+        <button className="add-to-cart" onClick={handleAddInCartButton}>
           <BsCartPlus />
         </button>
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,18 +2,33 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
 import { FaShoppingCart } from "react-icons/fa";
-import "./Navbar.css";
-import { useCartPortalContext, useContactContext } from "src/context";
+import { UserEnum } from "src/types/user.types";
+import {
+  useCartPortalContext,
+  useContactContext,
+  useUserTypeContext,
+} from "src/context";
 import { ContactPortal } from "../PortalComponents/ContactPortal/ContactPortal";
 import { CartPortal } from "../PortalComponents/CartPortal/CartPortal";
+import "./Navbar.css";
 
 export const Navbar = () => {
   const { isContactOpen, setIsContactOpen } = useContactContext();
   const { isCartOpen, setIsCartOpen } = useCartPortalContext();
+  const { userType, setUserType } = useUserTypeContext();
   const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  const handleLoginButton = () => {
+    navigate("/login");
+  };
+  const handleLogOutButton = () => {
+    navigate("/");
+    setUserType(UserEnum.GUEST);
+    localStorage.clear();
+  };
   return (
     <nav className="nav-bar">
       <div className={isNavbarOpen ? "in-nav-bar open" : "in-nav-bar"}>
@@ -68,15 +83,27 @@ export const Navbar = () => {
             <div className="product-quantity">0</div>
           </li>
         </motion.ul>
-        <motion.button
-          className="login-button"
-          initial={{ opacity: 0, x: 200 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 200 }}
-          onClick={() => navigate("/login")}
-        >
-          <FiLogIn className="login-icon" /> <span>Login</span>
-        </motion.button>
+        {userType === UserEnum.GUEST ? (
+          <motion.button
+            className="login-button"
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            onClick={handleLoginButton}
+          >
+            <FiLogIn className="login-icon" /> <span>Login</span>
+          </motion.button>
+        ) : (
+          <motion.button
+            className="login-button"
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            onClick={handleLogOutButton}
+          >
+            <FiLogOut className="login-icon" /> <span>Logout</span>
+          </motion.button>
+        )}
       </div>
       {isContactOpen && <ContactPortal />}
       {isCartOpen && <CartPortal />}
